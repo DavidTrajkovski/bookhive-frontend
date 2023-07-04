82963b645645b6924b4401a70d6d1d7d0ca6e606
david <davidtrajkovski11@gmail.com>
2023-07-05 00:35:05 +0200

    multiple modules and angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SharedModule} from "./shared/shared.module";
+import {CoreModule} from "./core/core.module";
+import {AppsModule} from "./apps/apps.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    CoreModule,
+    AppsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
