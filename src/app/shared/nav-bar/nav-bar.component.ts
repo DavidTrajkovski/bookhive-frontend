8d96a8cbe156364acb71fdeb1348a56b60e72b21
dev-marko <markospasenovski00@gmail.com>
2023-07-31 00:59:06 +0200

    Create Navbar component

---
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { AppRoutingModule } from '../../app-routing.module';
+import { RouteConstants } from '../RouteConstants';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss'],
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    AppRoutingModule,
+  ],
+})
+export class NavBarComponent {
+  home: string = RouteConstants.HOME;
+  books: string = RouteConstants.BOOKS;
+  login: string = RouteConstants.LOGIN;
+  register: string = RouteConstants.REGISTER;
+}
