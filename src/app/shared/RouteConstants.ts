65be51daea88d51e1d5a051043f177729ebd9793
dev-marko <markospasenovski00@gmail.com>
2023-07-31 01:00:12 +0200

    Update app-routing.module

--- src/app/shared/RouteConstants.ts
+++ src/app/shared/RouteConstants.ts
@@ -2,4 +2,5 @@ export class RouteConstants {
   public static readonly HOME = 'home';
   public static readonly LOGIN = 'login';
   public static readonly REGISTER = 'register';
+  public static readonly BOOKS = 'books';
 }
