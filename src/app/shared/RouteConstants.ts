aae09cb784f7d021c5e6b2d367e671aa4be6b153
dev-marko <markospasenovski00@gmail.com>
2023-08-07 22:31:29 +0200

    Add Bookshops route constant

--- src/app/shared/RouteConstants.ts
+++ src/app/shared/RouteConstants.ts
@@ -3,4 +3,5 @@ export class RouteConstants {
   public static readonly LOGIN = 'login';
   public static readonly REGISTER = 'register';
   public static readonly BOOKS = 'books';
+  public static readonly BOOKSHOPS = 'bookshops';
 }
