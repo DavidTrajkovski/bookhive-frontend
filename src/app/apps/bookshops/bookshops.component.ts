104092a8af6be6a19a5f6f41df022b62ad020877
dev-marko <markospasenovski00@gmail.com>
2023-08-07 22:30:22 +0200

    Create Bookshops page

---
+++ src/app/apps/bookshops/bookshops.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookshops',
+  templateUrl: './bookshops.component.html',
+  styleUrls: ['./bookshops.component.scss']
+})
+export class BookshopsComponent {
+
+}
