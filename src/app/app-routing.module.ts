51a1e0882c83e97292681ff0ea964006def8c661
dev-marko <markospasenovski00@gmail.com>
2023-08-07 22:30:57 +0200

    Add active link color for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,19 @@ import { RouteConstants } from './shared/RouteConstants';
 import { LoginPage } from './apps/login/login-page.component';
 import { LayoutComponent } from './shared/layout/layout.component';
 import { BooksComponent } from './apps/books/books.component';
+import { BookshopsComponent } from './apps/bookshops/bookshops.component';
+import { HomeComponent } from './apps/home/home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: RouteConstants.HOME, pathMatch: 'full' },
   {
     path: '',
     component: LayoutComponent,
-    children: [{ path: RouteConstants.BOOKS, component: BooksComponent }],
+    children: [
+      { path: RouteConstants.HOME, component: HomeComponent },
+      { path: RouteConstants.BOOKS, component: BooksComponent },
+      { path: RouteConstants.BOOKSHOPS, component: BookshopsComponent },
+    ],
   },
   { path: RouteConstants.REGISTER, component: RegisterPage },
   { path: RouteConstants.LOGIN, component: LoginPage },
