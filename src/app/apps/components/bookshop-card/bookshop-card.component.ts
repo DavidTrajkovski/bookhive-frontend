de8b875ff80d11b89152dd801865ea59a9340b5a
dev-marko <markospasenovski00@gmail.com>
2023-08-09 19:47:09 +0200

    Make bookshop-card dynamic

--- src/app/apps/components/bookshop-card/bookshop-card.component.ts
+++ src/app/apps/components/bookshop-card/bookshop-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Bookshop } from 'src/app/core/interface/bookshop';
 
 @Component({
   selector: 'bh-bookshop-card',
   templateUrl: './bookshop-card.component.html',
-  styleUrls: ['./bookshop-card.component.scss']
+  styleUrls: ['./bookshop-card.component.scss'],
 })
 export class BookshopCardComponent {
-
+  @Input() bookshop?: Bookshop;
 }
