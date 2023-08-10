8cab4c3467a65b827e8d8d5e3dcfb9bb10e940e5
dev-marko <markospasenovski00@gmail.com>
2023-08-10 23:02:55 +0200

    Create bookshop-details page component

--- src/app/apps/bookshop-details/bookshop-details.component.ts
+++ src/app/apps/bookshop-details/bookshop-details.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Bookshop } from 'src/app/core/interface/bookshop';
+import { BookshopBook } from 'src/app/core/interface/bookshop-book';
 import { BookshopService } from 'src/app/core/service/bookshop.service';
 import { RouteConstants } from 'src/app/shared/RouteConstants';
 
@@ -9,9 +10,13 @@ import { RouteConstants } from 'src/app/shared/RouteConstants';
   templateUrl: './bookshop-details.component.html',
   styleUrls: ['./bookshop-details.component.scss'],
 })
-export class BookshopDetailsComponent implements OnInit {
+export class BookshopDetailsComponent {
   bookshopId: string = '';
-  bookshops: Bookshop[] = [];
+  bookshop?: Bookshop;
+  bookshopIsLoading: boolean = true;
+
+  books: BookshopBook[] = [];
+  booksAreLoading: boolean = true;
 
   constructor(
     private route: ActivatedRoute,
@@ -19,17 +24,32 @@ export class BookshopDetailsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.getBookshopIdFromPath();
+    this.getBookshopById();
+    this.getBooksForBookshop();
+  }
+
+  getBookshopIdFromPath() {
     this.route.params.subscribe((params) => {
       this.bookshopId = params[RouteConstants.BOOKSHOPS_ID];
     });
-    console.log(this.getAllBookshops())
   }
 
-  getAllBookshops() {
-    this.bookshopService.getAllBookshops().subscribe({
+  getBookshopById() {
+    this.bookshopService.getBookshopById(this.bookshopId).subscribe({
       next: (data) => {
-        this.bookshops = data;
-        console.log(this.bookshops);
+        this.bookshop = data;
+        this.bookshopIsLoading = false;
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  getBooksForBookshop() {
+    this.bookshopService.getBooksForBookshop(this.bookshopId).subscribe({
+      next: (data) => {
+        this.books = data;
+        this.booksAreLoading = false;
       },
       error: (err) => console.error(err),
     });
