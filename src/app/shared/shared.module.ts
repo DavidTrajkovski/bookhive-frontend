624373aabd8dfa4acd893ce79358982b517884f3
dev-marko <markospasenovski00@gmail.com>
2023-08-10 23:02:33 +0200

    Create custom text-icon component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,22 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LayoutComponent } from './layout/layout.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { TextIconComponent } from './components/text-icon/text-icon.component';
 
 @NgModule({
-  declarations: [LayoutComponent],
-  imports: [NavBarComponent, AppRoutingModule, MatProgressSpinnerModule],
-  exports: [NavBarComponent, LayoutComponent, MatProgressSpinnerModule],
+  declarations: [LayoutComponent, TextIconComponent],
+  imports: [
+    NavBarComponent,
+    AppRoutingModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+  ],
+  exports: [
+    NavBarComponent,
+    LayoutComponent,
+    MatProgressSpinnerModule,
+    TextIconComponent,
+  ],
 })
 export class SharedModule {}
