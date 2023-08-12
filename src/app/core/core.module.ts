3cfeb24d5c86f335738b59a9e6e4930c97c9712b
dev-marko <markospasenovski00@gmail.com>
2023-08-12 16:23:37 +0200

    Merge services into one authentication service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RegisterService } from './service/authorization/register.service';
-import { LoginService } from './service/authorization/login.service';
 import { BookshopService } from './service/bookshop.service';
 import { SharedModule } from '../shared/shared.module';
-import { JwtService } from './service/authorization/jwt.service';
+import { AuthService } from './service/authentication/auth.service';
 
 @NgModule({
   imports: [SharedModule],
-  providers: [RegisterService, LoginService, BookshopService]
+  providers: [AuthService, BookshopService],
 })
 export class CoreModule {}
--- src/app/core/service/authentication/auth.service.ts
+++ src/app/core/service/authentication/auth.service.ts
@@ -1,16 +1,41 @@
 import { Injectable } from '@angular/core';
-import jwt_decode from 'jwt-decode';
-import * as moment from 'moment';
+import { Observable, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { LoginRequest } from '../../interface/authorization/login-request';
 import { LocalStorageService } from 'src/app/shared/services/local-storage.service';
+import { environment } from 'src/environments/environment.development';
+import { LoginResponse } from '../../interface/authorization/login-response';
+import { RegisterRequest } from '../../interface/authorization/register-request';
+import * as moment from 'moment';
+import jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
 })
-export class JwtService {
+export class AuthService {
+  baseUrl: string = `${environment.apiUrl}`;
   jwtToken: string | null = this.localStorageService.getItem('TOKEN');
   decodedToken: { [key: string]: string } = {};
 
-  constructor(private localStorageService: LocalStorageService) {}
+  constructor(
+    private _http: HttpClient,
+    private localStorageService: LocalStorageService
+  ) {}
+
+  login(loginRequest: LoginRequest): Observable<LoginResponse> {
+    return this._http
+      .post<LoginResponse>(`${this.baseUrl}/login`, loginRequest)
+      .pipe(
+        tap((data) => {
+          this.localStorageService.setItem('TOKEN', data.token);
+          this.setToken(data.token);
+        })
+      );
+  }
+
+  register(registerRequest: RegisterRequest): Observable<number> {
+    return this._http.post<number>(`${this.baseUrl}/register`, registerRequest);
+  }
 
   setToken(token: string): void {
     if (token) {
@@ -48,6 +73,7 @@ export class JwtService {
   }
 
   isLoggedIn(): boolean {
+    if (!this.jwtToken) return false;
     return moment().isBefore(this.getExpiration());
   }
 
