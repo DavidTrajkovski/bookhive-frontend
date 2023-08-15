64a1525c5357959a0438eea8e7591f249f393836
dev-marko <markospasenovski00@gmail.com>
2023-08-15 23:13:38 +0200

    Create initial Invitations component

---
+++ src/app/apps/bookclubs/invitations/invitations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invitations',
+  templateUrl: './invitations.component.html',
+  styleUrls: ['./invitations.component.scss']
+})
+export class InvitationsComponent {
+
+}
