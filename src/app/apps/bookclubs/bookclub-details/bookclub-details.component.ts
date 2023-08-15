3296260d969027a5dbdcea361c38f5d0bd9b4b90
dev-marko <markospasenovski00@gmail.com>
2023-08-15 23:13:27 +0200

    Create initial BookClubDetails component

---
+++ src/app/apps/bookclubs/bookclub-details/bookclub-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookclub-details',
+  templateUrl: './bookclub-details.component.html',
+  styleUrls: ['./bookclub-details.component.scss']
+})
+export class BookclubDetailsComponent {
+
+}
