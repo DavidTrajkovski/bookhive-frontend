7bad3e9c3e2c548f4c2b7a70e325a73fcbe9dab0
dev-marko <markospasenovski00@gmail.com>
2023-08-16 22:58:37 +0200

    Begin implementing topic-details component

--- src/app/apps/bookclubs/topic-details/topic-details.component.ts
+++ src/app/apps/bookclubs/topic-details/topic-details.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BookClub } from 'src/app/core/interface/bookclub/bookclub';
+import { Topic } from 'src/app/core/interface/bookclub/topic';
+import { BookclubService } from 'src/app/core/service/bookclub.service';
+import { RouteConstants } from 'src/app/shared/RouteConstants';
 
 @Component({
-  selector: 'app-topic-details',
+  selector: 'bh-topic-details',
   templateUrl: './topic-details.component.html',
-  styleUrls: ['./topic-details.component.scss']
+  styleUrls: ['./topic-details.component.scss'],
 })
 export class TopicDetailsComponent {
+  topicId: string = '';
+  topic?: Topic;
+  bookclub?: BookClub; // Needed for the breadcrumbs
 
+  constructor(
+    private route: ActivatedRoute,
+    private bookclubService: BookclubService
+  ) {}
+
+  ngOnInit(): void {
+    this.getTopicIdFromPath();
+  }
+
+  getTopicIdFromPath() {
+    this.route.params.subscribe((params) => {
+      this.topicId = params[RouteConstants.TOPIC_ID];
+    });
+  }
+
+  getBookClubById() {
+    this.bookclubService.getBookClubById(this.topic!.bookclubId).subscribe({
+      next: (data) => {
+        this.bookclub = data;
+      },
+      error: (err) => console.error(err),
+    });
+  }
 }
