f7e271d67a43367dec071a57cf77f7f2e9af9682
dev-marko <markospasenovski00@gmail.com>
2023-08-20 23:53:28 +0200

    Subscribe to all observables in invitation.service

--- src/app/core/service/invitation.service.ts
+++ src/app/core/service/invitation.service.ts
@@ -20,29 +20,53 @@ export class InvitationService {
     return this.http.get<number>(`${this.baseUrl}/count`);
   }
 
+  getMembershipRequestsForBookClub(
+    bookclubId: string
+  ): Observable<Invitation[]> {
+    return this.http.get<Invitation[]>(
+      `${this.baseUrl}/${bookclubId}/membership-requests`
+    );
+  }
+
   getRequestsCountForBookClub(bookclubId: string): Observable<number> {
     return this.http.get<number>(
       `${this.baseUrl}/${bookclubId}/requests-count`
     );
   }
 
+  requestBookClubMembership(bookclubId: string): void {
+    this.http
+      .post(`${this.baseUrl}/${bookclubId}/request-membership`, '')
+      .subscribe();
+  }
+
   sendInvitation(newInvitation: CreateNewInvitation): void {
-    this.http.post(`${this.baseUrl}/send-invitation`, newInvitation);
+    this.http
+      .post(`${this.baseUrl}/send-invitation`, newInvitation)
+      .subscribe();
   }
 
   acceptInvtitation(invitationId: string): void {
-    this.http.post(`${this.baseUrl}/${invitationId}/accept-invitation`, '');
+    this.http
+      .post(`${this.baseUrl}/${invitationId}/accept-invitation`, '')
+      .subscribe();
   }
 
   declineInvitation(invitationId: string): void {
-    this.http.delete(`${this.baseUrl}/${invitationId}/decline-invitation`);
+    this.http
+      .delete(`${this.baseUrl}/${invitationId}/decline-invitation`)
+      .subscribe();
   }
 
   acceptRequest(invitationId: string): void {
-    this.http.post(`${this.baseUrl}/${invitationId}/accept-request`, '');
+    this.http
+      .post(`${this.baseUrl}/${invitationId}/accept-request`, '')
+      .subscribe();
   }
 
   declineRequest(invitationId: string): void {
-    this.http.delete(`${this.baseUrl}/${invitationId}/decline-request`);
+    this.http
+      .delete(`${this.baseUrl}/${invitationId}/decline-request`)
+      .subscribe();
   }
 }
