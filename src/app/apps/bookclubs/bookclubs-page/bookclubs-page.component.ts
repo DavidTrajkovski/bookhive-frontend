1f54948b826cc6c4345108908e460e48eaec38dc
dev-marko <markospasenovski00@gmail.com>
2023-08-21 21:15:58 +0200

    Implement basic filtering by name for bookclubs

--- src/app/apps/bookclubs/bookclubs-page/bookclubs-page.component.ts
+++ src/app/apps/bookclubs/bookclubs-page/bookclubs-page.component.ts
@@ -5,10 +5,11 @@ import { BookclubService } from 'src/app/core/service/bookclub.service';
 @Component({
   selector: 'app-bookclubs-page',
   templateUrl: './bookclubs-page.component.html',
-  styleUrls: ['./bookclubs-page.component.scss']
+  styleUrls: ['./bookclubs-page.component.scss'],
 })
 export class BookclubsPageComponent {
   bookclubs: BookClub[] = [];
+  filteredBookclubs: BookClub[] = [];
   bookclubsAreLoading: boolean = true;
 
   constructor(private bookclubService: BookclubService) {}
@@ -17,10 +18,22 @@ export class BookclubsPageComponent {
     this.getAllBookshops();
   }
 
+  queryBookClubsByName(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    if (filterValue === '') {
+      this.filteredBookclubs = this.bookclubs;
+    } else {
+      this.filteredBookclubs = this.bookclubs.filter((item) =>
+        item.name.toLowerCase().includes(filterValue.trim().toLowerCase())
+      );
+    }
+  }
+
   getAllBookshops() {
     this.bookclubService.getAllBookClubs().subscribe({
       next: (data) => {
         this.bookclubs = data;
+        this.filteredBookclubs = this.bookclubs;
         this.bookclubsAreLoading = false;
       },
       error: (err) => console.error(err),
