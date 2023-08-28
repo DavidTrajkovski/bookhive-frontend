dd95a3aeab01d3b174ad44899a6630ced1ea354c
dev-marko <markospasenovski00@gmail.com>
2023-08-28 22:02:44 +0200

    Create shopping-cart.service

---
+++ src/app/core/service/shopping-cart.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { ShoppingCartInfo } from '../interface/shopping-cart/shopping-cart-info';
+import { AddToCard } from '../interface/shopping-cart/add-to-cart';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ShoppingCartService {
+  baseUrl: string = `${environment.apiUrl}/shopping-cart`;
+
+  constructor(private http: HttpClient) {}
+
+  getShoppingCartInfo(): Observable<ShoppingCartInfo> {
+    return this.http.get<ShoppingCartInfo>(this.baseUrl);
+  }
+
+  addBookToShoppingCart(addBookToShoppingCart: AddToCard): Observable<boolean> {
+    return this.http.post<boolean>(
+      `${this.baseUrl}/add`,
+      addBookToShoppingCart
+    );
+  }
+
+  removeBookFromShoppingCart(bookId: string) {
+    this.http.delete(this.baseUrl, { params: { bookId: bookId } });
+  }
+
+  payNow() {
+    // TODO: Impl
+  }
+}
